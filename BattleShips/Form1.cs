8232a3218e31cc8d966cf576fa993a58ec2e4046
Janek <jasrzewuski320@gmail.com>
2024-01-12 11:59:12 +0100

    Aktualizacja 12 Stycznia ( niedziała stawianie w pionie)

--- BattleShips/Form1.cs
+++ BattleShips/Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Runtime.InteropServices;
+
 namespace BattleShips
 {
     public partial class Form1 : Form
@@ -5,10 +8,12 @@ namespace BattleShips
         Board board = new Board();
         List<Ship> shipList = new List<Ship>();
         Random random = new Random();
-       
-        
+
+
+
         public Form1()
         {
+ 
             InitializeComponent();
         }
 
@@ -34,28 +39,130 @@ namespace BattleShips
             int x = random.Next(10);
             int y = random.Next(10);
             int orientation = random.Next(2);
-            int length = random.Next(5);
-            
+            int length = random.Next(4) + 1;
+
+            bool isCollision = false;
             
             Ship shipNew = new Ship(length, Color.Red, orientation, x, y);
 
+
             foreach (Ship ship in shipList)
             {
-                if (ship.PosX == shipNew.PosX && shipNew.PosY == ship.PosY)
+                if ((ship.Orientation == shipNew.Orientation) && (shipNew.Orientation == 0))
                 {
+                    if (shipNew.PosY == ship.PosY)
+                    {
+                        if (shipNew.PosX <= ship.PosX + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if (!(shipNew.PosX >= ship.PosX && shipNew.PosX < ship.PosX + ship.Length))
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if (shipNew.PosX + shipNew.Length > ship.PosX)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                    }
+                }
+
+
+
+                else if ((ship.Orientation == shipNew.Orientation) && (shipNew.Orientation == 1))
+                {
+                    if (shipNew.PosX == ship.PosX)
+                    {
+                        if (shipNew.PosY >= ship.PosY + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if (!(shipNew.PosY >= ship.PosY && shipNew.PosY < ship.PosY + ship.Length))
+                        {
+
+                            isCollision = true;
+                            break;
+                        }
+                        if (shipNew.PosY + shipNew.Length < ship.PosY)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        shipNew = new Ship(length, Color.Red, orientation, x, y);
+                    }
+                }
+                else if ((ship.Orientation != shipNew.Orientation) && (shipNew.Orientation == 1))
+                {
+                        if (shipNew.PosX + shipNew.Length == ship.PosX + ship.Length)
+                        {
+                        isCollision = true;
+                        break;
+                        }
+                        if (shipNew.PosX < ship.PosX + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+
+                        if (shipNew.PosX == ship.PosX)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if(shipNew.PosX + ship.Length > ship.PosX + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        
                     
                 }
-                else
+                else if ((ship.Orientation != shipNew.Orientation) && (shipNew.Orientation == 0))
                 {
+                        if (shipNew.PosY + shipNew.Length == ship.PosY + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if (shipNew.PosY < ship.PosY + ship.Length)
+                        {
+                            isCollision = true;
+                            break;
+                        }
 
+                        if(shipNew.PosY == ship.PosY)
+                        {
+                            isCollision = true;
+                            break;
+                        }
+                        if (shipNew.PosY + shipNew.Length > ship.PosY + ship.Length)
+                        {
+
+                            isCollision = true;
+                            break;
+                        }
+                
+
+                    
                 }
+
             }
-            shipList.Add(shipNew);
+
+            if (isCollision == false)
+            {
+                shipList.Add(shipNew);
+            }
 
 
 
+            //ship.PosX == shipNew.PosX && shipNew.PosY == ship.PosY
 
             pictureBox1.Invalidate();
         }
+
     }
 }
